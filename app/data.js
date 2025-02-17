ac3a006e89b8c4775aab7dba1177bc4c184e8e74
7trail <72806227+7trail@users.noreply.github.com>
2025-02-17 15:49:46 -0600

    substantial changes

---
+++ app/data.js
@@ -0,0 +1,3 @@
+'use server'
+const crypto = require('crypto-js');
+
---
+++ app/page.js
@@ -0,0 +1,15 @@
+'use client'
+import Image from "next/image";
+const crypto = require('crypto-js');
+
+function Message(message) {
+
+}
+
+export default function Home() {
+  return (
+    <div className = "main">
+      
+    </div>
+  );
+}
