4f98952b2bd3e98325f956de3d3ef96d0dbee009
7trail <72806227+7trail@users.noreply.github.com>
2025-02-17 15:52:02 -0600

    Update data.js

--- app/data.js
+++ app/data.js
@@ -1,3 +1,8 @@
 'use server'
 const crypto = require('crypto-js');
 
+var ciphertext = crypto.AES.encrypt(JSON.stringify(data), 'secret key 123').toString();
+
+// Decrypt
+var bytes  = crypto.AES.decrypt(ciphertext, 'secret key 123');
+var decryptedData = JSON.parse(bytes.toString(crypto.enc.Utf8));
